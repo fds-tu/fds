b68ac52f2728b5be5b5cd7dc7eb3ee3277c3084b
Lachezar Topalov <zari.topalov@gmail.com>
2025-04-19 20:43:08 +0300

    Add Base Relations

---
+++ backend/src/main/java/bg/tusofia/fcst/ksi/practikum/fds/entities/base/relations/ManyToMany.java
@@ -0,0 +1,15 @@
+package bg.tusofia.fcst.ksi.practikum.fds.entities.base.relations;
+
+import bg.tusofia.fcst.ksi.practikum.fds.entities.base.BaseEntity;
+import jakarta.persistence.*;
+
+@MappedSuperclass
+public abstract class ManyToMany<T1, T2> extends BaseEntity<Long> {
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "left_id")
+    private T1 one;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "right_id")
+    private T2 two;
+}
