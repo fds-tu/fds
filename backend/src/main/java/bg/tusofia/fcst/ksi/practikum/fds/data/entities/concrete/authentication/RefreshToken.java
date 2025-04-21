cc382a9be7ed9a8fc1fcfa6b71b393194ddb330d
Lachezar Topalov <zari.topalov@gmail.com>
2025-04-21 23:52:58 +0300

    Fix RefreshToken

--- backend/src/main/java/bg/tusofia/fcst/ksi/practikum/fds/data/entities/concrete/authentication/RefreshToken.java
+++ backend/src/main/java/bg/tusofia/fcst/ksi/practikum/fds/data/entities/concrete/authentication/RefreshToken.java
@@ -5,7 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Data
 public class RefreshToken extends BaseEntity<Long> {
