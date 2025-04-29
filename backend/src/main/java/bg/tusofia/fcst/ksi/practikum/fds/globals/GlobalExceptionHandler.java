9ea4522bde70030b4004109eee0db506311f4118
Lachezar Topalov <zari.topalov@gmail.com>
2025-04-29 23:28:09 +0300

    Add HttpMessageNotReadableException.class handling in GlobalExceptionHandler

--- backend/src/main/java/bg/tusofia/fcst/ksi/practikum/fds/globals/GlobalExceptionHandler.java
+++ backend/src/main/java/bg/tusofia/fcst/ksi/practikum/fds/globals/GlobalExceptionHandler.java
@@ -4,11 +4,14 @@ import bg.tusofia.fcst.ksi.practikum.fds.data.dtos.responses.ExceptionDto;
 import bg.tusofia.fcst.ksi.practikum.fds.data.dtos.responses.entities.ErrorResponseEntity;
 import bg.tusofia.fcst.ksi.practikum.fds.exceptions.rest.FdsRestException;
 import bg.tusofia.fcst.ksi.practikum.fds.exceptions.rest.InvalidResourceException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import jakarta.validation.ConstraintViolationException;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.internal.Pair;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -30,7 +33,7 @@ public class GlobalExceptionHandler {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getObjectName();
+            String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
 
@@ -48,6 +51,25 @@ public class GlobalExceptionHandler {
         });
 
         return this.handleException(new InvalidResourceException("Body", (Serializable) errors, true));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ErrorResponseEntity<ExceptionDto> handleException(HttpMessageNotReadableException ex) {
+        Throwable cause = ex.getCause();
+
+        if (cause instanceof JsonMappingException jsonMappingException) {
+            Map<String, String> errors = new HashMap<>();
+
+            jsonMappingException.getPath().stream()
+                    .map(ref -> Pair.of(ref.getFieldName(), String.format("%s is invalid", ref.getFieldName()) ))
+                    .forEach(pair -> errors.put(pair.getLeft(), pair.getRight()));
+
+
+            return this.handleException(new InvalidResourceException("Body", (Serializable) errors, true));
+        }
+
+        return this.handleException(new InvalidResourceException("Body", null, true));
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
